6082ba951dcf5061a807bbd2764672220193eda7
GunEng <gunkels@inbox.lv>
2022-09-21 17:56:53 +0300

    atjaunots

--- main.py
+++ main.py
@@ -18,9 +18,29 @@ def BMW():
 def Mercedes():
     return render_template('Mercedes.html')
 
-@app.route('/Loterija')
-def Loterija():
-    return render_template('Loterija.html')
+
+
+@app.route('/forma', methods = ['GET', 'POST'])
+def forma():
+    if request.method == 'GET':
+        return render_template('forma.html')
+
+    elif request.method == 'POST':
+        firstname = request.form.get('first-name')
+        lastname = request.form.get('last-name')
+        email = request.form.get('email')
+        password = request.form.get('new-password')
+
+        dataline = f"{firstname},{lastname},{email},{password}\n"
+
+        with open("users.csv", "a", encoding="utf-8") as f:
+            f.write(dataline)
+
+        return render_template('forma.html', message = "Paldies, jūsu atbilde pieņemta!")
+
+    else:   
+        return "ERROR"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
